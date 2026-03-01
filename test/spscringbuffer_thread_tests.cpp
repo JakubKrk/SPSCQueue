2bd0ca5891f74e7c707622da9e683fd5151fcdb1
Kuba <jakubkurek.pl@gmail.com>
2026-03-01 23:10:48 +0100

    Add push_wait and pop_wait tests

--- test/spscringbuffer_thread_tests.cpp
+++ test/spscringbuffer_thread_tests.cpp
@@ -108,3 +108,53 @@ TEST(SpscRingBufferThreaded, ConsumerStartsEarly)
     consumer.join();
     producer.join();
 }
+
+TEST(SpscRingBufferThreaded, PushWaitPopWaitOrdering)
+{
+    constexpr uint64_t N = 100000;
+    SpscRingBuffer<Message> buf{1024};
+
+    std::thread producer([&] {
+        for (uint64_t i = 0; i < N; ++i)
+        {
+            Message msg{"msg", i};
+            buf.push_wait(std::move(msg));
+        }
+    });
+
+    std::thread consumer([&] {
+        for (uint64_t i = 0; i < N; ++i)
+        {
+            Message msg = buf.pop_wait();
+            EXPECT_EQ(msg._value, i);
+        }
+    });
+
+    producer.join();
+    consumer.join();
+}
+
+TEST(SpscRingBufferThreaded, PushWaitBlocksUntilSpaceAvailable)
+{
+    constexpr std::size_t CAP = 4;
+    SpscRingBuffer<Message> buf{CAP};
+
+    for (uint64_t i = 0; i < CAP; ++i)
+        ASSERT_TRUE(buf.try_push(Message{"x", i}));
+
+    std::thread producer([&] {
+        Message msg{"extra", CAP};
+        buf.push_wait(std::move(msg));
+    });
+
+    std::thread consumer([&] {
+        for (uint64_t i = 0; i < CAP + 1; ++i)
+        {
+            Message msg = buf.pop_wait();
+            EXPECT_EQ(msg._value, i);
+        }
+    });
+
+    producer.join();
+    consumer.join();
+}
